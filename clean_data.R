2539178cf7b82209f6af8357969a5105d94f94a9
ritagior <rgiordano@gmx.com>
2022-11-04 06:47:32 +0000

    new website

--- clean_data.R
+++
@@ -1,9 +0,0 @@
-## cleand world fertility rate for flourish
-library(tidyverse)
-library(readxl)
-data <- read_excel("../../DataViz_TheGuardian/Fertility Rate Data.xlsx")
-fertility <- data %>%
-  filter(!is.na(`2019`)) %>%
-  select(`Country Name`, Region, IncomeGroup, `2019`)
-
-write_csv(fertility, "data/fertility_2019.csv")
